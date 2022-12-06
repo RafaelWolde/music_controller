314f29d0bf8bb282b8f6af506392c9a9d06b280b
RafaelWolde <rafaelwolde2030@gmail.com>
2022-12-06 14:32:57 +0300

    12/6/2022, 2:37. Changing computer

--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Button from '@material-ui/core'
+import Button from '@material-ui/core/Button'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import FormHelperText from '@material-ui/core/FormHelperText'
@@ -7,7 +7,8 @@ import FormControl from '@material-ui/core/FormControl'
 import Radio from '@material-ui/core/Radio'
 import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
-
+import TextField from "@material-ui/core/TextField";
+import { Link } from "react-router-dom";
 export default function CreateRoomPage(props) {
     const [state, setState] = React.useState(4)
     let defaultVotes = 2;
@@ -15,7 +16,7 @@ export default function CreateRoomPage(props) {
     return (
         <Grid container spacing={1}>
             <Grid item xs={12} align="center">
-                <Typography component="h4" variant='4' >
+                <Typography component="h4" variant='h4' >
                     Create A Room
                 </Typography>
             </Grid>
@@ -26,7 +27,7 @@ export default function CreateRoomPage(props) {
                             Guest Control Playback State
                         </div>
                     </FormHelperText>
-                    <RadioGroup row defaultValue={true}>
+                    <RadioGroup row defaultValue='true'>
                         <FormControlLabel
                             value={true}
                             control={<Radio color="primary" />}
@@ -39,9 +40,32 @@ export default function CreateRoomPage(props) {
                             label="No Control"
                             labelPlacement="bottom"
                         />
+
                     </RadioGroup>
+                    <FormControl>
+                        <TextField required={true}
+                            type={defaultVotes}
+                            inputProps={
+                                {
+                                    min: 1,
+                                    style: { textAlign: 'center' }
+
+                                }
+                            }>
+                        </TextField>
+                        <FormHelperText>
+                            <div align="center">Votes Required To Skip Song</div>
+                        </FormHelperText>
+                    </FormControl>
                 </FormControl>
             </Grid>
+            <Grid item xs={12} align="center">
+                <Button color="primary" variant="contained"> Create Room</Button>
+            </Grid>
+            <Grid item xs={12} align="center">
+                <Button color="secondary" variant="contained" to='/'> Back</Button>
+            </Grid>
+
         </Grid>
     )
 }
